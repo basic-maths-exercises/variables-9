f68f98f695d1d3ef5ae1ee173c5967498bac17bd
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-24 18:04:47 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+table = 7
+timesTable = np.zeros(11)
+
+timesTable[0] = 0*table
+timesTable[1]  = 1*table
+timesTable[2]  = 2*table
+timesTable[3]  = 3*table
+timesTable[4]  = 4*table
+timesTable[5]  = 5*table
+timesTable[6]  = 6*table
+timesTable[7]  = 7*table
+timesTable[8]  = 8*table
+timesTable[9]  = 9*table
+timesTable[10]  = 10*table
+
+print(table)
+print(timesTable)
